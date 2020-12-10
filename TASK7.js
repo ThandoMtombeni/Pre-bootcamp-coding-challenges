6123de6ed6032e60c442a15b40563aad0e3405e9
Thando <61787201+ThandoMtombeni@users.noreply.github.com>
2020-12-10 11:02:19 +0200

    Task7.js

--- TASK7.js
+++ TASK7.js
@@ -1,12 +1,12 @@
 //Task 7
-function tempConvert(temp){
+function celsiusToFahrenheit(temp){
     var Fahrenheit = (temp * 9/5) + 32
     return Fahrenheit;
 }
 tempConvert();
 
-function tempConvert2(tempF){
-   var celsius = (tempF - 32)*5/9
+function fahrenheitToCelsius(temp){
+   var celsius = (temp - 32)*5/9
    return celsius;
 }
 tempConvert2();
