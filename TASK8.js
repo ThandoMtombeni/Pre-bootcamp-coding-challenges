aae6fd7027d746f207c40a2cae910f3dad678ff4
Thando <61787201+ThandoMtombeni@users.noreply.github.com>
2020-12-10 10:55:26 +0200

    Task8.js

--- TASK8.js
+++ TASK8.js
@@ -6,7 +6,10 @@ function time(num){
     //console.log(roundhours);
     var minutes = (hours - roundhours) * 60;
     var roundminutes = Math.round(minutes);
-     return roundhours + " hour(s) and " + roundminutes + " minute(s).";
+    if(roundhours > 1 && roundminutes> 1 ){return roundhours + " hours and " + roundminutes + " minutes";}
+    else if(roundhours > 1 && roundminutes <= 1 ){return roundhours + " hours and " + roundminutes + " minute.";}
+    else if(roundhours <= 1 && roundminutes > 1 ){return roundhours + " hour and " + roundminutes + " minutes.";}
+    else{return roundhours + " hour and " + roundminutes + " minute.";}
  
 }
 console.log(time(71));
