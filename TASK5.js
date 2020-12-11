36cc43deebc1b2abb9a9653449b0b24e07e3450a
Thando <61787201+ThandoMtombeni@users.noreply.github.com>
2020-12-11 16:46:15 +0200

    Task5

--- TASK5.js
+++ TASK5.js
@@ -1,7 +1,7 @@
 //Task 5
 function triangle(num1,num2,num3){
 
-    var s = 0.5*(num1 + num2 + num3);
+    var semiArea = 0.5*(num1 + num2 + num3);
 
     var area =Math.sqrt(s*(s-num1)*(s-num2)*(s-num3));
     return area;
